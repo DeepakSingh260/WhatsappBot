c788383872d803eaf3a61892c50847469aac2885
DeepakSingh260 <alexmha260@gmail.com>
2023-07-02 14:14:06 +0530

    first commit

--- functions/firebase.js
+++ functions/firebase.js
@@ -7,13 +7,7 @@ const { getAnalytics } = require("firebase/analytics");
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyACZ5ZIIMFR8i29HWuANknutzWbNQ0ow9Y",
-  authDomain: "whatsappbankbot.firebaseapp.com",
-  projectId: "whatsappbankbot",
-  storageBucket: "whatsappbankbot.appspot.com",
-  messagingSenderId: "896436288781",
-  appId: "1:896436288781:web:ad2938fe6bf31fa2554444",
-  measurementId: "G-WGC6HK7REK"
+  
 };
 
 // Initialize Firebase
